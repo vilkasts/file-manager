026fc9e73c80137c739882818506c7452e018f0d
vilkasts <vilkasts@gmail.com>
2024-10-13 19:13:11 +0200

    fix: add file existing check to cp.js

--- src/utils/fs/cp.js
+++ src/utils/fs/cp.js
@@ -1,18 +1,22 @@
-import { createReadStream, createWriteStream } from 'node:fs'
 import { pipeline } from 'node:stream/promises'
+import { access } from 'node:fs/promises'
+import { createReadStream, createWriteStream } from 'node:fs'
 import { basename, join, resolve } from 'node:path'
+import constants from 'node:constants'
 
 const cp = async (filePath, targetDirPath) => {
   const resolvedFilePath = resolve(filePath ?? '')
   const resolvedTargetDirPath = resolve(targetDirPath ?? '')
   const newFilePath = join(resolvedTargetDirPath, basename(resolvedFilePath))
   
-  const readStream = createReadStream(resolvedFilePath)
-  const writeStream = createWriteStream(newFilePath, { flags: 'wx' })
-  
   try {
+    await access(resolvedFilePath, constants.F_OK);
+    
+    const readStream = createReadStream(resolvedFilePath)
+    const writeStream = createWriteStream(newFilePath, { flags: 'wx' })
+    
     await pipeline(readStream, writeStream)
-  } catch (error) {
+  } catch {
     console.error(`\nInvalid input: File already exists or incorrect file path or destination folder path\n`)
   }
 }
